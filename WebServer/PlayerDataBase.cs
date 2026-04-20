ab362412686bb948d6b1bdc15a284090b6673457
Jesse <Jessepan123456@gmail.com>
2026-04-20 14:24:37 -0600

    We think it works

---
+++ WebServer/PlayerDataBase.cs
@@ -0,0 +1,31 @@
+﻿namespace WebServerDemo
+{
+    public class PlayerDataBase
+    {
+        public int PlayerId { get; set; }
+        public string? PlayerName { get; set; }
+        public int MaxScore { get; set; }
+        public string? EnterTime { get; set; }
+        public string? EndTime { get; set; }
+
+        public PlayerDataBase()
+        {
+            PlayerId = 0;
+            PlayerName = "";
+            MaxScore = 0;
+            EnterTime = "";
+            EndTime = "";
+        
+        }
+
+        public PlayerDataBase(int playerId, string playerName, int maxScore, string enterTime, string endTime )
+        {
+            PlayerId = playerId;
+            PlayerName = playerName;
+            MaxScore = maxScore;
+            EnterTime = enterTime;
+            EndTime = endTime;
+        }
+    
+    }
+}
--- WebServer/WebServer.cs
+++ WebServer/WebServer.cs
@@ -9,7 +9,7 @@ using Networking;
 
 public static class WebServer
 {
-    public const string SqlConnection = "server=atr.eng.utah.edu;database=u1548814;uid=u1548814;password=bittermelon1";
+    private const string SqlConnection = "server=atr.eng.utah.edu;database=u1548814;uid=u1548814;password=bittermelon1";
 
     private const string HttpOkHeader = "HTTP/1.1 200 OK\r\n" +
                                         "Connection: close\r\n" +
@@ -19,6 +19,8 @@ public static class WebServer
     private const string HttpBadHeader =
         "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
 
+
+    private static Dictionary<int, PlayerDataBase> _playerList = new Dictionary<int, PlayerDataBase>();
     private static Dictionary<int, (string?, string?)> _gamesList = new Dictionary<int, (string?, string?)>();
 
     static void Main()
@@ -40,26 +42,10 @@ public static class WebServer
         {
             client.Send(HttpOkHeader +
                         "<html>\n  <h3>Welcome to the Snake Games Database!</h3>\n  <a href=\"/games\">View Games</a>\n</html>");
-
-            // This is a great start, but we need to programatically construct the HTML.
-            // You can do this by writing whatever c# code you want. For example, the database
-            // This code needs to query the database, find out who all is playing / what games exist, etc
-            /*
-             * <t>
-             * for loop:
-             *   For each game: <tr>
-             *      <td>reader["name"] </td> + " " + ...
-             *
-             */
         }
         else if (incomingMessage.Contains("GET /games "))
         {
-            string begining = HttpOkHeader +
-                              "<html>\\n  <table border=\\\"1\\\">\\n    <thead>\\n      <tr>\\n        <td>ID</td><td>Start</td><td>End</td>\\n      </tr>\\n    </thead>\\n    <tbody>\\n      <tr>\\n    ";
-            string ending =
-                "</tr>\n      ... (more table rows omitted for brevity) ...\n    </tbody>\n  </table>\n</html>";
             StringBuilder table = new StringBuilder();
-
             foreach (int id in _gamesList.Keys)
             {
                 table.Append(
@@ -70,10 +56,24 @@ public static class WebServer
 
             client.Send(HttpOkHeader + table.ToString());
         }
-        else if (incomingMessage.Contains("GET /games?gid=8 "))
+        else if (incomingMessage.Contains($"GET /games?gid="))
         {
-            client.Send(HttpOkHeader +
-                        "<html>\n  <h3>Stats for Game 8</h3>\n  <table border=\"1\">\n    <thead>\n      <tr>\n        <td>Player ID</td><td>Player Name</td><td>Max Score</td><td>Enter Time</td><td>Leave Time</td>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <td>12</td><td>Danny</td><td>1</td><td>11/23/2024 10:41:29 AM</td><td>11/23/2024 10:41:53 AM</td>\n      </tr>\n      ... (more table rows omitted for brevity) ...\n    </tbody>\n  </table>\n</html>");
+            _playerList.Clear();
+            char gameId = incomingMessage[15];
+            int id = int.Parse(gameId.ToString());
+            AllPlayer(id);
+            StringBuilder table = new StringBuilder();
+            table.Append(
+                $"<html>\n  <h3>Stats for Game {id}</h3>\n  <table border=\"1\">\n    <thead>\n      <tr>\n        <td>Player ID</td><td>Player Name</td><td>Max Score</td><td>Enter Time</td><td>Leave Time</td>\n      </tr>\n    </thead>\n    <tbody>");
+
+            foreach (int PlayerID in _playerList.Keys)
+            {
+                table.Append(
+                    $"<tr>\n        <td>{PlayerID}</td><td>{_playerList[PlayerID].PlayerName}</td><td>{_playerList[PlayerID].MaxScore}</td><td>{_playerList[PlayerID].EnterTime}</td><td>{_playerList[PlayerID].EndTime}</td>\n      </tr>");
+            }
+
+            table.Append(" </tbody>\n  </table>\n</html>");
+            client.Send(HttpOkHeader + table.ToString());
         }
         else
         {
@@ -85,26 +85,55 @@ public static class WebServer
 
     private static void AllGames()
     {
-        // Connect to the DB
         using (MySqlConnection conn = new MySqlConnection(SqlConnection))
         {
             try
             {
-                // Open a connection
                 conn.Open();
 
-                // Create a command
                 MySqlCommand command = conn.CreateCommand();
                 command.CommandText = "SELECT * FROM Games";
 
-                // Execute the command and cycle through the DataReader object
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader!.Read())
+                    {
+                        _gamesList.TryAdd(Convert.ToInt32(reader["ID"]),
+                            (reader?["StartTime"].ToString(), reader?["EndTime"].ToString()));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    private static void AllPlayer(int gameId)
+    {
+        using (MySqlConnection conn = new MySqlConnection(SqlConnection))
+        {
+            try
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "SELECT * FROM Players Where GameID='" + gameId + "'";
+                Console.Write(command.CommandText);
+
                 using (MySqlDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        //Console.WriteLine(reader["ID"] + " " + reader["StartTime"] + " " + reader["EndTime"]);
-                        _gamesList.TryAdd(Convert.ToInt32(reader["ID"]),
-                            (reader?["StartTime"].ToString(), reader?["EndTime"].ToString()));
+                        PlayerDataBase player = new PlayerDataBase(
+                            Convert.ToInt32(reader["ID"]),
+                            reader["name"].ToString()!,
+                            Convert.ToInt32(reader["MaxScore"]),
+                            reader["EnterTime"].ToString()!,
+                            reader["EndTime"].ToString()!
+                        );
+                        _playerList.TryAdd((Convert.ToInt32(reader["ID"])), player);
                     }
                 }
             }
